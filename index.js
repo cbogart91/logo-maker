08f6a7b883218b5c8d4e785214bcec4b693e8375
cbogart91 <166065354+cbogart91@users.noreply.github.com>
2024-07-17 09:07:53 -0500

    code working

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const { Circle, Square, Triangle } = require('./library/shapes');
 
-inquirer
-  .prompt({
+
+const questions = [
+    {
     type: 'input',
     name: 'username',
-    message: 'Enter a Github Username:',
-  })
-  // promises are chained to directly pass inquirer data into fetch request
-  .then((res) => fetch(`https://api.github.com/users/${res.username}`))
-  // promises are chained to parse the request for the json data
-  .then((res) => res.json())
-  // json data is accepted as user and logged to the console
-  .then((user) => console.log(user));
+    message: 'Please enter three characters'
+  },
+  {
+    type: 'list',
+    name: 'textColor',
+    message: 'Please select a color',
+    choices: ['Red', 'Green', 'Yellow']
+  },
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Please select a shape',
+    choices: ['Circle', 'Triangle', 'Square']
+  },
+  // {
+  //   type: 'input',
+  //   name: 'shapeColor',
+  //   message: 'Please enter the color for the shape'
+  // },
+];
+
+inquirer
+.prompt(questions)
+.then((answers) => {
+  let shape;
+  if (answers.shape === 'circle'){
+    shape = new Circle()
+  }
+  if (answers.shape === 'square'){
+    shape = new Square()
+  }
+  if (answers.shape === 'triangle'){
+    shape = new Triangle()
+  }
+  // shape.setColor(answers.shapeColor)
+
+  const svgFile = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+ 
+   <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+  </svg>`
+
+
+  fs.writeFile('SVG-Generator.svg', svgFile, (err) => {
+    if (err) throw err;
+    console.log('File was saved!');
+  });
+})
+
+
+
---
+++ library/shapes.js
@@ -0,0 +1,38 @@
+class Shape {
+    constructor() {
+        this.color = " "
+    }
+    setColor(color) {
+        this.color = color;
+    }
+}
+
+class Circle extends Shape {
+    constructor() {
+        super();
+    }
+        render() {
+        return '<circle cx="150" cy="100" r="95" fill="${this.color}"/>'
+    }
+};
+
+class Square extends Shape {
+    constructor(){
+        super();
+    }
+    render() {
+        return '<rect x="55" y="10" width="200" height="200" fill="${this.color}"/>'
+    }
+};
+
+class Triangle extends Shape {
+    constructor() {
+        super();
+    }
+    render() {
+        return '<polygon points="150 10, 275 190, 25 190" fill="${this.color}"/>'
+    }
+};
+
+
+module.exports = { Circle, Square, Triangle };
